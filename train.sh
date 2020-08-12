de1e96913900e9a6fe231d9bebcfc2729e224adf
Herbie Bradley <mail@herbiebradley.com>
2020-08-12 15:55:30 +0100

    Better train.sh

--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@
 # Arg2: Grad lambda.
 
 python train_GAN.py --run "${1}-${2}" --grad_lambda "${2}"
-python fid/fid_score.py --log_dir "log/${1}/generated_images" --gpu "0" --output_name "${1}"
+python calculate_fid.py --log_dir "log/${1}/generated_images" --output_name "${1}"
 python cifar10_train.py --run "${1}" --data_dir "log/${1}/generated_images"
