db807b5b6f0e9fa8f6c8714022d5b909f5afcff9
Herbie Bradley <mail@herbiebradley.com>
2020-08-12 15:42:15 +0100

    Better train script

--- train.sh
+++ train.sh
@@ -2,6 +2,6 @@
 # Arg1: Name of run/model, will be used as log dir name.
 # Arg2: Grad lambda.
 
-python train_GAN.py --run "${1}" --grad_lambda "${2}"
+python train_GAN.py --run "${1}-${2}" --grad_lambda "${2}"
 python fid/fid_score.py --log_dir "log/${1}/generated_images" --gpu "0" --output_name "${1}"
 python cifar10_train.py --run "${1}" --data_dir "log/${1}/generated_images"
