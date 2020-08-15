e648b7b7edbd8c3364f16f973c02326285b949e8
Herbie Bradley <mail@herbiebradley.com>
2020-08-15 21:45:46 +0100

    Fix train.sh

--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@
 # Arg2: Grad lambda.
 
 python train_GAN.py --run "${1}-${2}" --grad_lambda "${2}"
-python calculate_fid.py --log_dir "log/${1}-${2}" --output_name "${1}"
+python calculate_fid.py --log_dir "log/${1}-${2}" --output_name "${1}-${2}"
 python cifar10_train.py --run "${1}-${2}" --data_dir "log/${1}-${2}/generated_images"
